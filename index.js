840163171f957d4235d3b08c63e5945c059bdcc9
flexdevguy <flexdevguy@gmail.com>
2019-07-30 19:12:29 +0530

    Flex layout changes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Flex',
+      gettingStarted:'Getting Started with Flex Layout..'
     };
   }
 
@@ -16,8 +17,23 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Getting Started with React
+          {this.state.gettingStarted + ` ${this.state.name}`}
         </p>
+        <div class="flex-container">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+        </div>
+
+        <div class="flex-center-container">
+          <button type="button">Click Me</button>
+        </div>
       </div>
     );
   }
